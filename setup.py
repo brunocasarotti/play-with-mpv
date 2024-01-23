7a2ccf93f7f31d8b33052774f3856eead830199d
Bruno Casarotti <brunoo.casarotti@gmail.com>
2024-01-23 10:01:07 -0300

    install_freedesktop
    install_freedesktop>=0.1.2

--- setup.py
+++ setup.py
@@ -47,9 +47,9 @@ setup(
             'play-with-mpv=play_with_mpv:start',
         ],
     },
-    setup_requires=['wheel', 'install_freedesktop>=0.2.0'],
+    setup_requires=['wheel', 'install_freedesktop>=0.1.2'],
     dependency_links=[
-        "https://github.com/thann/install_freedesktop/tarball/master#egg=install_freedesktop-0.2.0"
+        "https://github.com/thann/install_freedesktop/tarball/master#egg=install_freedesktop-0.1.2"
     ],
     desktop_entries={
         'play-with-mpv': {
